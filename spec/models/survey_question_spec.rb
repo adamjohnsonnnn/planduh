7c52f37c36424580a1d81f047de7112a093ca156
Mitch Mele <mitchmele@gmail.com>
2017-12-04 06:11:59 -0600

    Add specs for user, preferences, and survey questions.

--- spec/models/survey_question_spec.rb
+++ spec/models/survey_question_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SurveyQuestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:preference) {SurveyQuestion.new(:question_text => 'Do you have any dietary restrictions?', :version => "Open-ended")}
+
+
+  describe "Survey Responses" do
+
+     it "a Survey question has a question for the user" do
+         expect(preference.question_text).to eq "Do you have any dietary restrictions?"
+     end
+
+     it "the survey question type is a string" do
+         expect(preference.question_text).to be_a(String)
+     end
+  end
+
 end
