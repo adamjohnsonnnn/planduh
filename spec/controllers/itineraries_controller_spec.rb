1c8f579de336ba7112d7ce1f584b20bd4ad0b556
Mitch Mele <mitchmele@gmail.com>
2017-12-05 16:33:51 -0600

    update tests

--- spec/controllers/itineraries_controller_spec.rb
+++ spec/controllers/itineraries_controller_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe ItinerariesController, type: :controller do
   let(:itinerary) { Itinerary.create!(name: "Paris", location: "1033 W Van Buren, Chicago IL", :user_id => user.id, :confirmed? => true, :budget => 120, :date => "17/12/11", :begin_time => "22:09", :end_time => "12:00" )}
 
  describe "GET #index" do
-    it "responds with status code 200" do
+    it "responds with status code 302" do
       get :new
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
   end
  end
 
