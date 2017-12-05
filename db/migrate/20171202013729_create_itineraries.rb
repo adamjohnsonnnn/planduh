28ae9a103ee3b8c8b1aeb94c04a02ada4d5ec1d9
Zach Broaddus <zach.broaddus@gmail.com>
2017-12-05 15:03:33 -0600

    set ride destination as first event location

--- db/migrate/20171202013729_create_itineraries.rb
+++ db/migrate/20171202013729_create_itineraries.rb
@@ -2,6 +2,11 @@ class CreateItineraries < ActiveRecord::Migration[5.1]
   def change
     create_table :itineraries do |t|
       t.string :name
+      t.date :date
+      t.text :begin_time
+      t.text :end_time
+      t.integer :budget
+      t.string  :location
       t.boolean :confirmed?
       t.references :user
 
