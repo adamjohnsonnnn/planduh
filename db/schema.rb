7aca0776aa63258527e59f433cc1bf1456b9eda2
Zach Broaddus <zach.broaddus@gmail.com>
2017-12-05 18:54:12 -0600

    add migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203001021) do
+ActiveRecord::Schema.define(version: 20171206004821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20171203001021) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "agreed"
   end
 
   create_table "yelp_responses", force: :cascade do |t|
